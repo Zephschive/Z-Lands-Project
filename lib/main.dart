c049e7c47e70a33d5cab09477d90a7cd99b365b5
Zephschive <zephaniahollennu@gmail.com>
2024-02-14 14:18:04 +0000

    committed

--- lib/main.dart
+++ lib/main.dart
@@ -22,11 +22,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Zlands Frontend',
+      title: 'Zlands ',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
       
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        
         useMaterial3: true,
       ),
       home:SplashView(),
